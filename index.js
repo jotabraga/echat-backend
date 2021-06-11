67671b83b475ea0f1e2d33bdf930d8c634901a80
jotabraga <jpmbraga.feg@gmail.com>
2021-06-11 16:53:42 -0300

    kick out the inative participants

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 dayjs().format();
-
 const chatMemory = fs.existsSync("./chatMemory.json")
 ? JSON.parse(fs.readFileSync("./chatMemory.json"))
 : { participants: [], messages: [] };
+let messages = chatMemory.messages;
+let participants = chatMemory.participants;
 //{from:,to:,text:,type:,time:}
 
 app.post('/participants',(req,res) =>{
 
-    if(req.body.name !== "" || req.body.name !== null){
-        {participants.name.includes(req.body.name) 
+    let username = req.body.name;
+
+    if(username !== "" || username !== null)
+    {
+        participants.name.includes(username) 
             ? 
             res.send("O nome do usuário já está em uso") 
             :
@@ -24,8 +28,8 @@ app.post('/participants',(req,res) =>{
             momentOfArrival = Date.now();
             participants.push({...req.body, "lastStatus": momentOfArrival});
             res.sendStatus(200);
-        }
-    }else{
+    }
+    else{
         res.sendStatus(400);
     }
 })
@@ -34,6 +38,8 @@ function sendMsgOfArrival(req){
     message = {from: req.body.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: 'HH:MM:SS'};
     messages.push(messageOfArrival);
 }
+
+
 
 app.get('/participants',(req,res) => {
     if(participants.name.includes(req.headers.user)){
@@ -63,5 +69,12 @@ app.post('/status',(req,res) =>{
     participants.push(req.body);
     res.send();
 })
+
+function saveData() {
+    fs.writeFileSync(
+        "./data/chatData.json",
+        JSON.stringify({ participants, messages })
+    );
+}
 
 app.listen(4000);
