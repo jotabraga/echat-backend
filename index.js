1ea09ebb7f8a80ad2531d2bd17f8e68fb48fc4a6
jotabraga <jpmbraga.feg@gmail.com>
2021-06-11 12:38:56 -0300

    If username is in use return a message

--- index.js
+++ index.js
@@ -5,19 +5,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const participants = [{name:,lastStatus:}];
+const participants = [];
 const messages = [{from:,to:,text:,type:,time:}];
 
 app.post('/participants',(req,res) =>{
-    const newId = (posts[posts.length-1].id + 1);
-    posts.push({...req.body, "id":newId});
-    res.send();
+    {participants.includes(req.body.name) 
+        ? 
+        res.send("O nome do usuário já está em uso") 
+        :
+        momentOfArrival = Date.now();
+        participants.push({...req.body, "lastStatus": momentOfArrival});
+        res.send();
+    }
 })
 
 app.get('/participants',(req,res) => {
+    res.send(posts.find((post) => post.id == postId));
+});
+
+app.post('/messages',(req,res) =>{
+    participants.push(req.body);
+    res.send();
+})
+
+app.get('/messages',(req,res) => {
     const postId = req.params.id;
     res.send(posts.find((post) => post.id == postId));
 });
 
+app.post('/status',(req,res) =>{
+    participants.push(req.body);
+    res.send();
+})
 
 app.listen(4000);
