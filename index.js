f7ace0305e06d88342766262efe00cf7804a9cca
jotabraga <jpmbraga.feg@gmail.com>
2021-06-11 13:53:53 -0300

    memory space for chat messages and participants

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import dayjs from 'dayjs';
+import fs from 'fs';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dayjs().format();
 
-const participants = [];
-const messages = [{from:,to:,text:,type:,time:}];
+const chatMemory = fs.existsSync("./chatMemory.json")
+? JSON.parse(fs.readFileSync("./chatMemory.json"))
+: { participants: [], messages: [] };
+//{from:,to:,text:,type:,time:}
 
 app.post('/participants',(req,res) =>{
-    
+
     if(req.body.name !== "" || req.body.name !== null){
         {participants.name.includes(req.body.name) 
             ? 
             res.send("O nome do usuário já está em uso") 
             :
+            sendMsgOfArrival(); 
             momentOfArrival = Date.now();
             participants.push({...req.body, "lastStatus": momentOfArrival});
-            res.send();
+            res.sendStatus(200);
         }
+    }else{
+        res.sendStatus(400);
     }
 })
+
+function sendMsgOfArrival(req){
+    message = {from: req.body.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: 'HH:MM:SS'};
+    messages.push(messageOfArrival);
+}
 
 app.get('/participants',(req,res) => {
     if(participants.name.includes(req.headers.user)){
         res.send(participants);        
     }else{
-        res.status(400).send("Houve um erro, tente novamente");
+        res.sendStatus(400);
     }
 });
 
