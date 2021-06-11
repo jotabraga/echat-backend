04997a6b092549365c372b8471e203ae8e8fa019
jotabraga <jpmbraga.feg@gmail.com>
2021-06-11 00:48:16 -0300

    Server structure of posts and gets used along the application

--- index.js
+++ index.js
@@ -8,19 +8,16 @@ app.use(express.json());
 const participants = [{name:,lastStatus:}];
 const messages = [{from:,to:,text:,type:,time:}];
 
-
-app.get('/posts/',(req,res) => {
-    const postId = req.params.id;
-    res.send(posts.find((post) => post.id == postId));
-});
-
-
-app.post('/new-story',(req,res) =>{
+app.post('/participants',(req,res) =>{
     const newId = (posts[posts.length-1].id + 1);
     posts.push({...req.body, "id":newId});
     res.send();
 })
 
+app.get('/participants',(req,res) => {
+    const postId = req.params.id;
+    res.send(posts.find((post) => post.id == postId));
+});
 
 
 app.listen(4000);
